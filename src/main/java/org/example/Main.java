0f5e55f453d3fa809d0db8aee606a8758d8a6f9e
Dmitriy <9269085062@bk.ru>
2025-08-20 10:36:49 +0300

    * Task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,28 @@
 package org.example;
 
+import java.util.List;
+import java.util.Map;
 
 import lombok.SneakyThrows;
+import org.example.enums.TestResult;
+import org.example.processors.TestDetail;
+import org.example.processors.TestRunner;
+import org.example.testClasses.ExampleTest;
 
 public class Main {
+
     @SneakyThrows
     public static void main(String[] args) {
+        Map<TestResult, List<TestDetail>> results = TestRunner.runTests(ExampleTest.class);
 
+        for (Map.Entry<TestResult, List<TestDetail>> entry : results.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue().size());
+            for (TestDetail detail : entry.getValue()) {
+                System.out.println(" ---------------- " + detail.testName() +
+                    (detail.exception() != null ?
+                        " (" + detail.exception().getMessage() + ")" : ""));
+            }
+        }
     }
 }
 
